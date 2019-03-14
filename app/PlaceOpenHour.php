1bbecc9d570a7a42a0ea031fbbb33bc39cd74b02
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-03-14 11:58:29 +0100

    Add PlaceOpenHours model and migration

---
+++ app/PlaceOpenHour.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Traits\UsesUuid;
+use Illuminate\Database\Eloquent\Model;
+
+class PlaceOpenHour extends Model
+{
+    use UsesUuid;
+}
---
+++ database/migrations/2019_03_14_102951_create_place_open_hours_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaceOpenHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('place_open_hours', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('place_id');
+            $table->foreign('place_id')->references('id')->on('places')->onDelete('cascade');
+            $table->tinyInteger('weekday'); // 0 is monday, 6 is sunday
+            $table->time('time_from')->nullable()->default(null);
+            $table->time('time_to')->nullable()->default(null);
+            $table->date('special_hours_date')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('place_open_hours');
+    }
+}
