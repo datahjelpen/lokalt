76a7a86ddb1df58d8760d242c6aed3083cd6b203
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-03-14 11:59:38 +0100

    Add Place, PlaceOpenHours, PlaceUser and Address factories scaffold

---
+++ database/factories/PlaceOpenHourFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\PlaceOpenHour::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/PlaceUserFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\PlaceUser::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
