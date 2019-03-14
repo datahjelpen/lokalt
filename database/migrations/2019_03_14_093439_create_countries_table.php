48f9b5475e39cb1bc11a258914d10e4fa413d01a
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-03-14 11:56:06 +0100

    Add Country model and migration

---
+++ database/migrations/2019_03_14_093439_create_countries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name_short_local')->nullable()->default(null);
+            $table->string('name_short_international');
+            $table->string('name_official_local')->nullable()->default(null);
+            $table->string('name_official_international')->nullable()->default(null);
+            $table->string('code_iso')->unique();
+            $table->string('code_call');
+            $table->boolean('eu_member')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
