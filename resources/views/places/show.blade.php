417af29a1e0571417ad639e647a424c64c36927f
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-03-17 00:27:35 +0100

    Update place show view

--- resources/views/places/show.blade.php
+++ resources/views/places/show.blade.php
@@ -2,23 +2,50 @@
 
 @section('content')
 <h1>{{ $place->name }}</h1>
-<p>{!! nl2br( htmlspecialchars($place->description) ) !!}</p>
-<p>
-    <span>{{ $place->address->street_name_number }},</span>
-    <span>{{ $place->address->postal_code }} {{ $place->address->postal_city }}</span>
-</p>
 <ul>
-    <li>{{ $place->website }}</li>
-    <li>{{ $place->phone }}</li>
-    <li>{{ $place->email }}</li>
+    @if ($place->website)
+        <li><a href="{{ $place->website }}" target="_blank" ref="noopener nofollow noreferrer">{{ $place->website }}</a></li>
+    @endif
+    @if ($place->phone)
+        <li><a href="tel:{{ $place->phone }}">{{ $place->phone }}</a></li>
+    @endif
+    @if ($place->email)
+        <li><a href="mailto:{{ $place->email }}">{{ $place->email }}</a></li>
+    @endif
+    <li>
+        <a href="{{ url('https://www.google.com/maps/search/' .  $place->address->street_name_number . ' ' . $place->address->postal_code . ' ' . $place->address->postal_city) }}"
+            target="_blank"
+            ref="noopener nofollow noreferrer"
+        >
+            <span>{{ $place->address->street_name_number }},</span>
+            <span>{{ $place->address->postal_code }} {{ $place->address->postal_city }}</span>
+        </a>
+    </li>
 </ul>
-<h2>{{ __('Opening hours') }}</h2>
-@php
-    $i = 0;
-@endphp
+<p>{!! nl2br( htmlspecialchars($place->description) ) !!}</p>
+
+@if (count($place->opening_hours_regular) > 0)
+    <h2>{{ __('Opening hours') }}</h2>
+    @php
+        $i = 0;
+    @endphp
+    @foreach ($weekdays as $weekday)
+        <p><strong>{{ $weekday }}:</strong> {{ $place->opening_hours->regular[++$i]->timeFromTo() }}</p>
+    @endforeach
+@endif
+
+@if (count($place->opening_hours->special) > 0)
+    <hr>
+    <h3>Spesielle tider</h3>
+    @foreach ($place->opening_hours->special as $special_hour)
+        <p><strong>{{ $weekdays[$special_hour->weekday] }} {{ $special_hour->special_hours_date }}:</strong> {{ $special_hour->timeFromTo(false) }}</p>
+    @endforeach
+@endif
+
+
+{{--
 @foreach ($weekdays as $weekday)
 @php
-    $i++;
     $open_from = null;
     $open_to = null;
     $day_is_open = false;
@@ -46,5 +73,5 @@
         <span>Stengt</span>
     @endif
 </p>
-@endforeach
+@endforeach --}}
 @endsection
