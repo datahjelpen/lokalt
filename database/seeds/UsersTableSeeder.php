964bdd5774e2580840fea4bfaa0d1f172d5887f0
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-03-17 23:56:12 +0100

    Seed for admin user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use Carbon\Carbon;
+
 use App\User;
 
 class UsersTableSeeder extends Seeder
@@ -12,6 +16,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $user = new User;
+        $user->name = 'Bjørnar';
+        $user->email = 'bjornar@datahjelpen.no';
+        $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'; // password
+        $user->email_verified_at = Carbon::now();
+        $user->remember_token = Str::random(10);
+        $user->save();
+
+        $user->assignRole('superadmin');
+        $user->assignRole('admin');
+        $user->save();
+
         if (App::environment('local')) {
             factory(User::class, 50)->create();
         }
