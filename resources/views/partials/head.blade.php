f3a52278f6b5934dba712f8378852181b932dcee
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-03-25 14:31:49 +0100

    Fix css/js getting cache stuck

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,11 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name') }}</title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}?v=20210325" defer></script>
+    <link href="{{ asset('css/app.css') }}?v=20210325" rel="stylesheet">
 </head>
